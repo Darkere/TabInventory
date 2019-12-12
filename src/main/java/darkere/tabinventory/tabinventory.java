affb5f15ecf6149d08125248d1259e9e01f2cc44
Darkere <Dark-277@web.de>
2019-12-12 16:30:01 +0100

    Initial commit

---
+++ src/main/java/darkere/tabinventory/tabinventory.java
@@ -0,0 +1,44 @@
+package darkere.tabinventory;
+
+import com.google.common.eventbus.Subscribe;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.util.InputMappings;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.lwjgl.glfw.GLFW;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("tabinventory")
+public class tabinventory {
+    public tabinventory() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public void KeyBoardKeyPressed(GuiScreenEvent.KeyboardKeyPressedEvent event) {
+        if (event.getKeyCode() == GLFW.GLFW_KEY_TAB && Minecraft.getInstance().gameSettings.keyBindInventory.isActiveAndMatches(InputMappings.getInputByCode(event.getKeyCode(), event.getModifiers()))) {
+            if (!(event.getGui() instanceof ChatScreen)) {
+                Minecraft.getInstance().player.closeScreen();
+                event.setCanceled(true);
+            }
+        }
+    }
+}
