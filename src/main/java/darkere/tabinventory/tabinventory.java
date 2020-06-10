013a3d0d6f89c33aae3ab32b8d6f8eb0223bbf6f
Darkere <Dark-277@web.de>
2020-06-10 22:42:23 +0200

    disable in CommandBlock Screens

--- src/main/java/darkere/tabinventory/tabinventory.java
+++ src/main/java/darkere/tabinventory/tabinventory.java
@@ -1,35 +1,21 @@
 package darkere.tabinventory;
 
-import com.google.common.eventbus.Subscribe;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.AbstractCommandBlockScreen;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.util.InputMappings;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ExtensionPoint;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("tabinventory")
@@ -43,7 +29,7 @@ public class tabinventory {
     public void KeyBoardKeyPressed(GuiScreenEvent.KeyboardKeyPressedEvent event) {
         if(Minecraft.getInstance().player == null) return;
         if (event.getKeyCode() == GLFW.GLFW_KEY_TAB && Minecraft.getInstance().gameSettings.keyBindInventory.isActiveAndMatches(InputMappings.getInputByCode(event.getKeyCode(), event.getModifiers()))) {
-            if (!(event.getGui() instanceof ChatScreen)) {
+            if (!(event.getGui() instanceof ChatScreen) && !(event.getGui() instanceof AbstractCommandBlockScreen)) {
                 Minecraft.getInstance().player.closeScreen();
                 event.setCanceled(true);
             }
